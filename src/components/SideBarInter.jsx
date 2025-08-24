844f51da0420aac9f0933b50f090469858083d8c
gamortseye <68132212+gamortseye@users.noreply.github.com>
2025-08-24 20:18:09 +0000

    Update SideBarInter.jsx

--- src/components/SideBarInter.jsx
+++ src/components/SideBarInter.jsx
@@ -22,7 +22,7 @@ export default function Sidebar() {
             language: 'en',
             sortBy: 'publishedAt',
             pageSize: 6,
-            apiKey: process.env.VITE_NEWS_API_KEY, // store key in env
+            apiKey: import.meta.env.VITE_NEWS_API_KEY, // store key in env
           },
         });
         setArticles(data.articles ?? []);
